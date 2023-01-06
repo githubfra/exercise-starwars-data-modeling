d2e9fac7c8d710c44808fef0cbe191078144bc43
githubfra <diazcabellofrancisca@gmail.com>
2023-01-06 16:14:44 +0000

    add diagram

--- src/models.py
+++ src/models.py
@@ -8,26 +8,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User (Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    email = Column(String(250))
+    password = Column(String(10))
+    favorites = relationship("Favorite")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Favorite (Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    def to_dict(self):
-        return {}
+class Character (Base):
+    __tablename__ = "characters"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    homeworld = Column(String(100))
+    vehicle = Column(String(100))
+    planet = relationship("Planet")
+    vehicle = relationship("Vehicle")
+
+class Planet (Base):
+    __tablename__ = "planets"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    population = Column(Integer)
+    character_id = Column (Integer, ForeignKey("characters.id"))
+
+class Vehicle (Base):
+    __tablename__ = "vehicles"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    crew = Column(Integer)
+    character_id = Column (Integer, ForeignKey("characters.id"))
+
+
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
